9700dae809bbdb667df21ff5f2a9b2b2fd3cf9a8
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-11-13 09:56:21 -0600

    Value update

--- modules/legislation/server/controllers/client.legislation.controller.js
+++ modules/legislation/server/controllers/client.legislation.controller.js
@@ -71,23 +71,40 @@ exports.addPositiveVote = function( req, res ){
 
     console.log('Add positive vote...');
 
-    // Updates the information in the db
-    Legislation.findOneAndUpdate(
-        // Update query
-        { _id : legislation },
-        // Update operation
-        { $inc: { positiveVotes : 1 } })
-        .exec(function (updateError, rawError) {
-            // Validate any possible execution error
-            if (updateError) {
+    Legislation
+        .findOne({id: legislation})
+        .lean()
+        .exec(function (err, legislations) {
+            // Checking for any errors..
+            if (err) {
                 return res.status(400).send({
-                    message: errorHandler.getErrorMessage(updateError)
+                    message: errorHandler.getErrorMessage(err)
                 });
             }
 
-            return res.status(200);
-        })
-    ;
+            console.log('Get legislation..');
+
+
+            // Updates the information in the db
+            Legislation.update(
+                // Update query
+                { _id : legislation },
+                // Update operation
+                { $set: { positiveVotes : legislations.positiveVotes + 1  } })
+                .exec(function (updateError, rawError) {
+                    // Validate any possible execution error
+                    if (updateError) {
+                        return res.status(400).send({
+                            message: errorHandler.getErrorMessage(updateError)
+                        });
+                    }
+
+                    console.log('Update...');
+
+                    return res.status(200);
+                });
+        });
+
 };
 
 /**
